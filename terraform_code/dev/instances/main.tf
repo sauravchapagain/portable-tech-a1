3537e8855cdb91db43468d3ec6ce0e146fca1ed2
saurav <schapagain6@myseneca.ca>
2024-06-01 17:02:55 +0000

    minor typo corrected

--- terraform_code/dev/instances/main.tf
+++ terraform_code/dev/instances/main.tf
@@ -115,6 +115,6 @@ resource "aws_ecr_repository" "my_app" {
   name = "ass1-prod-app"
 }
 
-resource "aws_ecr_repository2" "my_db" {
+resource "aws_ecr_repository" "my_db" {
   name = "ass1-prod-db"
 }
